34e5fdd6ec26972ffa87f4f6e45114d240594252
kang88666 <na2nghermawan@yahoo.com>
2026-06-11 15:41:40 +0700

    chore: add Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,26 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+        ],
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
